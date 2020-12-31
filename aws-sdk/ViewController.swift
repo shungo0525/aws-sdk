83314740eb4ccf10433a913c36221a05a57d7847
shungo0525 <shungo0525@gmail.com>
2020-12-31 12:14:32 +0900

    initial commit

---
+++ aws-sdk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aws-sdk
+//
+//  Created by SHUNGO on 2020/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
